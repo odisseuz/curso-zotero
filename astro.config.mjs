3befadad1ad15896b2d00a41e45b80d0704c2c98
eulaptop <andregomes.academico@gmail.com>
2026-07-18 15:01:58 -0300

    Trabalhando na introducao e mod 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,23 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Curso CIPET II',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/odisseuz/curso-zotero' }],
-			sidebar: [
-				{
-					label: 'Módulos',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Parte 1', slug: 'modulos/parte_1' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'Curso CIPET II',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/odisseuz/curso-zotero' }],
+            sidebar: [
+                {
+                    label: 'Módulo 1',
+                    items: [
+                        // Caminho: pasta/arquivo
+                        { label: 'Introdução', slug: 'modulo_1/introducao' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    items: [{ autogenerate: { directory: 'reference' } }],
+                },
+            ],
+        }),
+    ],
 });
