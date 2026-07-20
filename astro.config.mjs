703e9298ce6ad2929c8b5cc77f4b4d7c698298a8
CNix <andregomes.academico@gmail.com>
2026-07-20 14:06:11 -0300

    teste ghpages. Close #10

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://odisseuz.github.io",
+  base: "/curso-zotero",
   integrations: [
     starlight({
       title: "Curso CIPET II",
